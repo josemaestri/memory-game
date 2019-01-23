456fc5563fa218e81351798cd162572bc12e59df
josemaestri <maestri.jose@gmail.com>
2019-01-23 10:30:28 -0500

    imgs

--- src/components/Tile/Tile.js
+++ src/components/Tile/Tile.js
@@ -5,8 +5,8 @@ class Tile extends Component{
 
   render(props) {
     return (
-      <div className="card m-3 align-middle game-tile" onClick={()=> this.props.setCounted(this.props.id)}>
-        <img src={this.props.img} alt="" className="card-img-top"/>
+      <div className="card m-3 align-middle game-tile" onClick={()=> this.props.setCounted(this.props.id)} style={{backgroundImage: `url(${this.props.img})`}}>
+        
         
       </div>
     );

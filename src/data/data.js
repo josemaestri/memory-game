9cdae01c5c90ed05c59101fa800d051ac2a1b357
josemaestri <maestri.jose@gmail.com>
2019-01-23 09:55:42 -0500

    imgs

--- src/data/data.js
+++ src/data/data.js
@@ -4,73 +4,73 @@ const data = [
   {
     id:1,
     name:'Wakko',
-    img: '/assets/img/wakkowarner.jpg',
+    img: '../assets/img/wakkowarner.jpg',
     counted: false
   },
   {
     id:2,
     name:'Yakko',
-    img: '/assets/img/YakkoWarner.png',
+    img: '../assets/img/YakkoWarner.png',
     counted: false
   },
   {
     id:3,
     name:'Dot',
-    img: '/assets/img/dotwarner.jpg',
+    img: '../assets/img/dotwarner.jpg',
     counted: false
   },
   {
     id:4,
     name:'Pinky',
-    img: '/assets/img/Pinky.png',
+    img: '../assets/img/Pinky.png',
     counted: false
   },
   {
     id:5,
     name:'Brain',
-    img: '/assets/img/brain.jpg',
+    img: '../assets/img/brain.jpg',
     counted: false
   },
   {
     id:6,
     name:'Mr. Cory Skullhead',
-    img: '/assets/img/MrSkullhead.png',
+    img: '../assets/img/MrSkullhead.png',
     counted: false
   },
   {
     id:7,
     name:'Dr. Otto Scratchansniff',
-    img: '/assets/img/DrScratchansniff.jpg',
+    img: '../assets/img/DrScratchansniff.jpg',
     counted: false
   },
   {
     id:8,
     name:'Freakazoid',
-    img: '/assets/img/Freakazoid.jpg',
+    img: '../assets/img/Freakazoid.jpg',
     counted: false
   },
   {
     id:9,
     name:'Freakazette',
-    img: '/assets/img/Freakazette.jpg',
+    img: '../assets/img/Freakazette.jpg',
     counted: false
   },
   {
     id:10,
     name:'Dexter Douglas',
-    img: '/assets/img/Dexter.jpg',
+    img: '../assets/img/Dexter.jpg',
     counted: false
   },
   {
     id:11,
     name:'Minerva Mink',
-    img: '/assets/img/Minerva_Mink.png',
+    img: '../assets/img/Minerva_Mink.png',
     counted: false
   },
   {
     id:12,
     name:'Big Fat Baby',
-    img: '/assets/img/Big_Fat_Baby.jpg',
+    img: '../assets/img/Big_Fat_Baby.jpg',
     counted: false
   }
 ];
